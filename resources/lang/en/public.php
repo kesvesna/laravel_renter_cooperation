2fb54aa61bc0f405de40c653d8db891a8a5b1adb
kesvesna <kesvesna@rambler.ru>
2021-12-31 15:32:36 +0300

    Create login views Create login form translation on english

---
+++ resources/lang/en/public.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: note
+ * Date: 08.11.2020
+ * Time: 15:44
+ */
+
+return [
+    "login_title" => 'Login to your account',
+    "login_credentials" => 'Enter your credentials below',
+    "login_email" => 'Email address',
+    "login_password" => 'Password',
+    "login_button_text" => 'Login',
+
+];
