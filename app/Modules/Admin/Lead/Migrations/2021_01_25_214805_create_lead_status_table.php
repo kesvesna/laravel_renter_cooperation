d8a18f5974c26607b23903023f20e66840664560
kesvesna <kesvesna@rambler.ru>
2022-01-09 15:20:42 +0300

    Created lead create api

--- app/Modules/Admin/Lead/Migrations/2021_01_25_214805_create_lead_status_table.php
+++ app/Modules/Admin/Lead/Migrations/2021_01_25_214805_create_lead_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLeadCommentsTable extends Migration
+class CreateLeadStatusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,14 @@ class CreateLeadCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('lead_comments', function (Blueprint $table) {
+        Schema::create('lead_status', function (Blueprint $table) {
             $table->id();
 
-            $table->text('text');
-
-            $table->bigInteger('user_id')->unsigned()->nullable();
-            $table->foreign('user_id')->references('id')->on('users');
-
             $table->bigInteger('lead_id')->unsigned()->nullable();
-            $table->foreign('lead_id')->references('id')->on('leads');
-
             $table->bigInteger('status_id')->unsigned()->nullable();
+
+            $table->foreign('lead_id')->references('id')->on('leads');
             $table->foreign('status_id')->references('id')->on('statuses');
-
-            $table->text('comment_value')->nullable();
-
-            $table->boolean('is_event')->default(0);
 
             $table->timestamps();
             $table->softDeletes();
@@ -43,6 +34,6 @@ class CreateLeadCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lead_comments');
+        Schema::dropIfExists('lead_status');
     }
 }
--- app/Modules/Admin/User/Models/User.php
+++ app/Modules/Admin/User/Models/User.php
@@ -6,7 +6,7 @@ use App\Modules\Admin\Lead\Models\Traits\UserLeadsTrait;
 use App\Modules\Admin\Role\Models\Traits\UserRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as AuthUser;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends AuthUser
 {
@@ -15,13 +15,20 @@ class User extends AuthUser
     protected $fillable = [
         'firstname',
         'lastname',
-        'phone',
         'email',
-        'status'
+        'phone',
+        'status',
     ];
 
     protected $hidden = [
-        'password',
-        'deleted_at'
+        'password'
     ];
+
+    public function getFullnameAttribute() {
+        return $this->firtsname.' '.$this->lastname;
+    }
+
+
+
+
 }
